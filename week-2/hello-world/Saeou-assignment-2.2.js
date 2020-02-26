e882b8bfcbee50a69d943ec5214cd70252bd20df
Joann saeou <joannssaeou@gmail.com>
2020-02-26 16:25:30 -0600

    adding week-2 files

---
+++ week-2/hello-world/Saeou-assignment-2.2.js
@@ -0,0 +1,35 @@
+/*
+============================================
+; Title:  Saeou-assignment-2.2
+; Author: Joann Saeou
+; Date:   26 February 2020
+; Description: hello world with express week-2
+;===========================================
+*/
+
+//start program
+
+//returns a header 
+var header = require('../../Saeou-header');
+
+//outputting the header with my firstname, lastname, and assignment
+
+console.log(header.display("Joann", "Saeou", "Assignment-2.2"));
+
+// entering variable express with require syntax 
+var express = require('express');
+var http = require('http');
+
+
+var app = express();
+
+
+app.use(function(req, res) {
+    console.log('In comes a request to: %s', req.url); //outputting result into the console
+
+    res.end('Hello World\n');
+})
+
+http.createServer(app).listen(8080, function() {
+    console.log('Application started on port %s', 8080); //output result into the port server 8080
+})
